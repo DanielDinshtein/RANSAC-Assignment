4081b53a1aa347f0330d43a1ad3a919db3049262
DanielDinshtein <danieldinsh@gmail.com>
2022-03-26 20:41:00 +0300

    parallel_ransac - Code Working Now try optimize

--- src/main.py
+++ src/main.py
@@ -1,11 +1,42 @@
+import time as time
+
 from ransac_parallel import parallel_ransac
+
+from ransac_excercise import ransac, read_samples
 
 
 # ========= run ransac ==============
 
+
+def run_original():
+    start = time.time()
+
+    path_to_samples_csv = 'input files/samples_for_line_a_48.9684912365_b_44.234.csv'
+    samples = read_samples(path_to_samples_csv)
+
+    best_model = ransac(samples, iterations=5000, cutoff_dist=20)
+
+    end = time.time()
+    print("\n* Run Time {:.3f}\n".format(end - start))
+
+    print("Serial Algorithm model Stats: - \n")
+    print(best_model)
+
+    # now plot the model
+    # plot_model_and_samples(best_model, samples)
+
+
 def run_ransac():
+    start = time.time()
+
     path_to_samples_csv = "C:/Users/Daniel/Desktop/משרות/משימות מראיונות/Mobileye/RANSAC-Assignment/input files/samples_for_line_a_48.9684912365_b_44.234.csv"
     best_model = parallel_ransac(path_to_samples_csv, iterations=5000, cutoff_dist=20)
+
+    end = time.time()
+    print("\n* Run Time {:.3f}\n".format(end - start))
+
+    print("Parallel Algorithm model Stats: - \n")
+    print(best_model)
 
     # samples = read_samples(path_to_samples_csv)
     # best_model = parallel_ransac(samples, iterations=5000, cutoff_dist=20)
@@ -17,4 +48,5 @@ def run_ransac():
 # ============ main =================
 
 if __name__ == '__main__':
+    # run_original()
     run_ransac()
--- src/ransac_parallel.py
+++ src/ransac_parallel.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pyspark.sql.functions as F
 from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType
 
-from src.utils import init_spark, round_up_to_even
+from src.utils import init_spark, round_up_to_even, read_samples
 
 
 # =========    Parallel     ============
@@ -43,8 +43,6 @@ def init_models_DF(spark):
     :return: Empty Models Spark DF
     """
     modelsSchema = StructType([
-        StructField('x1', DoubleType()), StructField('y1', DoubleType()),
-        StructField('x2', DoubleType()), StructField('y2', DoubleType()),
         StructField('a', DoubleType()), StructField('b', DoubleType())
     ])
 
@@ -66,26 +64,24 @@ def get_random_sample_pairs(samples, num_of_pairs):
 
     random_sample_pairs_df = pd.DataFrame(columns=('x1', 'y1', 'x2', 'y2'))
 
-    while pairs_to_add != 0:
-        samples_to_take = round_up_to_even(pairs_to_add * 2.005)
+    while pairs_to_add > 0:
+        samples_to_take = round_up_to_even(pairs_to_add * 1.005)
+        #  TODO: Improve - remove
+        print("1")
 
-        random_samples = samples.rdd.takeSample(True, samples_to_take)
+        random_samples_1 = pd.DataFrame(samples.sample(n=samples_to_take, replace=True).values, columns=('x1', 'y1'))
+        random_samples_2 = pd.DataFrame(samples.sample(n=samples_to_take, replace=True).values, columns=('x2', 'y2'))
 
-        for row_idx_1 in range(samples_to_take // 2):
-            row_idx_2 = row_idx_1 + samples_to_take // 2
+        df = pd.concat([random_samples_1, random_samples_2], axis=1).reset_index(drop=True)
 
-            if random_samples[row_idx_1]['x'] - random_samples[row_idx_2]['x'] != 0:
-                random_sample_pairs_df = random_sample_pairs_df.append({
-                    "x1": random_samples[row_idx_1]['x'],
-                    "y1": random_samples[row_idx_1]['y'],
-                    "x2": random_samples[row_idx_2]['x'],
-                    "y2": random_samples[row_idx_2]['y'],
-                }, ignore_index=True)
+        random_sample_pairs_df = random_sample_pairs_df.append(df)
 
-                if num_of_pairs - len(random_sample_pairs_df) == 0:
-                    break
+        random_sample_pairs_df = random_sample_pairs_df[
+            random_sample_pairs_df.x1 - random_sample_pairs_df.x2 != 0].reset_index(drop=True)
 
         pairs_to_add = num_of_pairs - len(random_sample_pairs_df)
+
+    random_sample_pairs_df = random_sample_pairs_df.iloc[:num_of_pairs]
 
     return random_sample_pairs_df
 
@@ -117,6 +113,27 @@ def create_models_from_sample_pairs(sample_pairs):
     return models_data
 
 
+def calc_models_scores_against_samples(spark, samples, models, cutoff_dist=20):
+    models_df = spark.createDataFrame(models)
+
+
+    def calculate_score(model):
+        calc_DF = pd.DataFrame()
+
+        calc_DF['pred_y'] = model['a'] * samples['x'] + model['b']
+        calc_DF['distance'] = samples['y'] - calc_DF['pred_y']
+        calc_DF['score'] = [dis if dis <= cutoff_dist else cutoff_dist for dis in
+                            calc_DF['distance'].abs().values]
+        return calc_DF['score'].sum(), { 'a': model['a'], 'b': model['b'] }
+
+
+    totalScore = models_df.rdd.map(calculate_score)
+
+    result = totalScore.reduce(lambda model_a, model_b: model_a if model_a[0] <= model_b[0] else model_b)
+
+    return { 'model': result[1], 'score': result[0] }
+
+
 def scoreModelAgainstSamples(model, samples, cutoff_dist=20):
     # predict the y using the model and x samples, per sample, and sum the abs of the distances between the real y
     # with truncation of the error at distance cutoff_dist
@@ -141,147 +158,22 @@ def scoreModelAgainstSamples(model, samples, cutoff_dist=20):
 def parallel_ransac(file_path, iterations, cutoff_dist):
     spark, sc = init_spark()
 
-    samples_df = extract_data(spark, file_path)
+    # samples_df = extract_data(spark, file_path)
 
-    models_df = init_models_DF(spark)
+    samples_df = pd.DataFrame(read_samples(file_path))
 
     random_sample_pairs = get_random_sample_pairs(samples=samples_df, num_of_pairs=iterations)
 
-    create_models_from_sample_pairs(sample_pairs=random_sample_pairs)
+    models = create_models_from_sample_pairs(sample_pairs=random_sample_pairs)
+
+    result = calc_models_scores_against_samples(spark=spark, samples=samples_df, models=models, cutoff_dist=cutoff_dist)
 
     #  TODO:  Need This? & Remove
     # samples_df.persist()
     # samples_df.printSchema()
     # samples_df.show(truncate=False)
 
-    min_m = {}
+    min_m = { }
     min_score = -1
-    # row_list = samples_df.collect()
-    # for i in range(1, iterations):
-    #     if i % 10 == 0:
-    #         print(i)
-    #     # sample1, sample2 = get_random_sample_pair(samples_df)
-    #     # sample1, sample2 = get_random_sample_pair(row_list)
-    #     m = modelFromSamplePair(sample1, sample2)
-    #     score = scoreModelAgainstSamples(m, samples_df, cutoff_dist)
-    #
-    #     if min_score < 0 or score < min_score:
-    #         min_score = score
-    #         min_m = m
-    #
-    # print({'model': min_m, 'score': min_score})
 
-    return {'model': min_m, 'score': min_score}
-
-
-"""# =========    Serially     ============"""
-
-
-# function that picks a pair of random samples from the list of samples given (it also makes sure they do not have the same x)
-
-
-# def get_random_sample_pair(samples):
-#     dx = 0
-#     selected_samples = []
-#     while dx == 0:
-#         # keep going until we get a pair with dx != 0
-#         selected_samples = []
-#         for i in [0, 1]:
-#             index = random.randint(0, len(samples) - 1)
-#             x = samples[index]['x']
-#             y = samples[index]['y']
-#             selected_samples.append({'x': x, 'y': y})
-#             # print("creator_samples ",i, " : ", creator_samples, " index ", index)
-#         dx = selected_samples[0]['x'] - selected_samples[1]['x']
-#
-#     return selected_samples[0], selected_samples[1]
-
-
-# generate a line model (a,b) from a pair of (x,y) samples
-# sample1,2 -  is a dictionary with x and y keys
-# def modelFromSamplePair(sample1, sample2):
-#     dx = sample1['x'] - sample2['x']
-#     if dx == 0:  # avoid division by zero later
-#         dx = 0.0001
-#
-#     # model = <a,b> where y = ax+b
-#     # so given x1,y1 and x2,y2 =>
-#     #  y1 = a*x1 + b
-#     #  y2 = a*x2 + b
-#     #  y1-y2 = a*(x1 - x2) ==>  a = (y1-y2)/(x1-x2)
-#     #  b = y1 - a*x1
-#
-#     a = (sample1['y'] - sample2['y']) / dx
-#     b = sample1['y'] - sample1['x'] * a
-#     return {'a': a, 'b': b}
-
-
-# create a fit score between a list of samples and a model (a,b) - with the given cutoff distance
-# def scoreModelAgainstSamples(model, samples, cutoff_dist=20):
-#     # predict the y using the model and x samples, per sample, and sum the abs of the distances between the real y
-#     # with truncation of the error at distance cutoff_dist
-#
-#     totalScore = 0
-#     for sample_i in range(0, len(samples) - 1):
-#         sample = samples[sample_i]
-#         pred_y = model['a'] * sample['x'] + model['b']
-#         score = min(abs(sample['y'] - pred_y), cutoff_dist)
-#         totalScore += score
-#
-#     # print("model ",model, " score ", totalScore)
-#     return totalScore
-
-
-# the function that runs the ransac algorithm (serially)
-# gets as input the number of iterations to use and the cutoff_distance for the fit score
-# def parallel_ransac(samples, iterations, cutoff_dist):
-#     # runs ransac algorithm for the given amount of iterations, where in each iteration it:
-#     # 1. randomly creates a model from the samples by calling m = modelFromSamplesFunc(samples)
-#     # 2. calculating the score of the model against the sample set
-#     # 3. keeps the model with the best score
-#     # after all iterations are done - returns the best model and score
-#
-#     min_m = {}
-#     min_score = -1
-#     for i in range(1, iterations):
-#         if i % 10 == 0:
-#             print(i)
-#         sample1, sample2 = get_random_sample_pair(samples)
-#         m = modelFromSamplePair(sample1, sample2)
-#         score = scoreModelAgainstSamples(m, samples, cutoff_dist)
-#
-#         if min_score < 0 or score < min_score:
-#             min_score = score
-#             min_m = m
-#
-#     return {'model': min_m, 'score': min_score}
-
-
-# ======== some basic pyspark example ======
-def some_basic_pyspark_example():
-    from pyspark import SparkContext
-    num_cores_to_use = 8  # depends on how many cores you have locally. try 2X or 4X the amount of HW threads
-
-    # now we create a spark context in local mode (i.e - not on cluster)
-    sc = SparkContext("local[{}]".format(num_cores_to_use), "My First App")
-
-
-    # function we will use in parallel
-    def square_num(x):
-        return x * x
-
-
-    rdd_of_num = sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
-
-    rdd_of_num_squared = rdd_of_num.map(square_num)
-
-    sum_of_squares = rdd_of_num_squared.reduce(lambda a, b: a + b)
-
-    # if you want to use the DataFrame interface - you need to create a SparkSession around the spark context:
-    from pyspark.sql import SparkSession
-    session = SparkSession(sc)
-
-    # create dataframe from the rdd of the numbers (call the column my_numbers)
-    df = session.createDataFrame(rdd_of_num, ['my_numbers'])
-    df = df.withColumn('squares', df['my_numbers'] * df['my_numbers'])
-    sum_of_squares = df['squared'].sum()
+    return result
